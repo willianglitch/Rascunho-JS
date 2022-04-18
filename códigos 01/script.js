6447ef494be832e4122f03aaaf655d1d8fb3bdc5
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-04-18 12:16:04 -0300

    set atributo
    código 02 que muda imagens ao click.

--- códigos 01/script.js
+++ códigos 01/script.js
@@ -1,7 +1,26 @@
+// atribui-se um tamanho á uma imagem "-02-".
+
 let leafeon = document.getElementById("leaf");
 
 leafeon.addEventListener("click", function(){
 
    leafeon.setAttribute("height", "34px")
+});
 
+
+// muda imagens de uma para outra "-02-".
+
+let leafeon = document.getElementById("leaf");
+
+leafeon.addEventListener("click", function(){
+   
+   leafeon.setAttribute("src","./assets/evee.png")
+   
+   leafeon.addEventListener("click", mudarFoto)
+
+   function mudarFoto(){
+      leafeon.setAttribute("src","./assets/shandsherion.png")
+      leafeon.setAttribute("height", "350px")
+   }
+   
 });
