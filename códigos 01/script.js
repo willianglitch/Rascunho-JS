d849c2f1913c51c7147a1c716b904e1060dffffe
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-04-21 16:46:37 -0300

    update 1.0
    rascunho

--- códigos 01/script.js
+++ códigos 01/script.js
@@ -24,3 +24,28 @@ leafeon.addEventListener("click", function(){
    }
    
 });
+
+
+// lista  "-03-"
+// basicamente uma lista de elementos que foi tratado a maneira de 
+// pegar elementos e atribui elementos com o "lista.dataset.num".
+
+//  ó correto se eu for criar ou colocar um atributo 
+// desconhecido é utilizar ao invés de "num "
+//  o "data-num"
+// !!!! 
+
+// o que permite fazer isso , sendo tecnicamente a forma mais correta.
+
+let lista = document.getElementById("lista");
+lista.dataset.num = "12"
+let num = parseInt(lista.dataset.num);
+
+console.log();
+
+let conteúdo =""
+
+for( let i = 0 ; i < num ; i++){
+    conteúdo += "<li>" + i + "</li>";
+}
+lista.innerHTML = conteúdo;
